7895b21ffe4869679a0e46ed6d3de3ebd905e1da
Jihyeok Choi <jihyeok.choi@voyagerx.com>
2023-01-07 16:22:46 +0900

    connect electron

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite';
-import crossOriginIsolation from 'vite-plugin-cross-origin-isolation';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crossOriginIsolation()],
+  base: process.env.ELECTRON === 'true' ? './' : '/',
+  plugins: [
+    react(),
+    {
+      name: 'configure-response-headers',
+      configureServer: (server) => {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          next();
+        });
+      },
+    },
+  ],
 });
